76a9d3e815228bc8e7fa254786d9659e240f8c82
Vadim Brik <Brik99brik@gmail.com>
2017-02-13 23:34:24 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const Tesseract = require('tesseract.js')
+const image = require('path').resolve(__dirname, process.argv[2])
 
+Tesseract.recognize(image)
+    .then    ( data   => console.log(data.text))
+    .catch   ( err    => console.error(err))
+    .finally ( ()     => process.exit())
