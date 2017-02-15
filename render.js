0ece10fa24e58c1f8d55f7e408957df9721a8699
Brik Vadim <vadimbrik25@gmail.com>
2017-02-15 21:19:20 +0600

    Bug fixes

--- render.js
+++ render.js
@@ -1,26 +1,43 @@
 const Tesseract = require('tesseract.js')
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs')
 
-let filesList  = document.getElementById('filesList')
+let filesList = document.getElementById('filesList')
 let relustArea = document.getElementById('result')
 let image = null
 
-filesList.onchange = function () {
-    image = path.resolve(__dirname, filesList.value)
-
-    Tesseract.recognize(image)
+function recognizeImage(image) {
+    Tesseract.recognize(image, {
+            lang: 'rus'
+        })
         .then(data => relustArea.innerHTML = data.text)
         .catch(err => alert(err))
+}
+
+function fillFilesList(files) {
+    clearFileList()
+    files.forEach(file => addFileToList(file))
+}
+
+function addFileToList(fileName) {
+    let fileListItem = document.createElement("option")
+    fileListItem.appendChild(document.createTextNode(fileName))
+
+    filesList.appendChild(fileListItem)
+}
+
+function clearFileList() {
+    filesList.innerHTML = ''
+}
+
+filesList.onchange = function () {
+    image = path.resolve(__dirname, filesList.value)
+    recognizeImage(image)
 }
 
 fs.readdir(__dirname, (err, files) => {
     if (err) {
         return console.error(err)
     }
-
-    files.forEach(file => {
-        console.log(file)
-        filesList.innerHTML += `<option>${file}</option>`
-    })
+    fillFilesList(files)
 })
